9dd5ae84ad2bde127005ce29718576a9bb58b2da
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2014-12-02 00:43:45 -0500

    new mail detection and last-notification erasure
    also last-notification writing

--- mailboxxx.py
+++ mailboxxx.py
@@ -24,20 +24,23 @@ class Mailroom(object):
         
 class Mailbox(object):
     def __init__(self, owner, mb_num, sensor_num, core):
-        self.owner = owner
+        self.owner = owner  # Owner object
         self.number = mb_num  # Mailbox number
         self.got_mail = False   #Boolean --yes mail\no mail
         self.sensor_num = str(sensor_num)   # Sensor number ('1', '2', ...)
         self.core = core    # Relevant spark core
-        self.lastchecked = None
-        self.lastnotification = None
+        self.lastchecked = None # Timestamp of last time we checked mailbox
+        self.lastnotification = None # Timestamp of last time we emailed owner.
+        self.newmail = None     # Has mail been added to a previously empty mailbox?
         
     def __str__(self):
+        """Returns a descriptive string including the mailbox number, it's owner, and the core
+        to which it is connected.
+        """
         return 'Mailbox %d belonging to %s (on Core "%s")' % (self.number, self.owner.name, self.core.name)
 
     def check(self):
         """Checks mailbox for mail using sensor input """
-        #self.got_mail = whatever we just found (Boolean)
         error = 0
         try:
             sensor = self.core.analogRead(self.sensor_num)
@@ -46,10 +49,29 @@ class Mailbox(object):
             # print 'Connection error'
             error = 1
         else:
+            previous_mail = self.got_mail
             if sensor > 2000:   # Mail threshold
                 self.got_mail = True
             else:
                 self.got_mail = False
+            if not previous_mail and self.got_mail: # If we didn't have mail, and now we do,
+                # Then we should have new mail.
+                self.newmail = True
+                # If we decide to NOT send reminder emails, then we may need only this and no 
+                # last notification time. This edge detection will send a notification once per 
+                # 'set' of mail. 
+                #
+                # Then again, it may be good to keep the last notification time and use it to check
+                # that our sensors aren't wigging out and spamming people with mail. 
+
+            elif previous_mail and not self.got_mail:   # If we had mail and now we don't, 
+                # Then they picked up their mail
+                self.lastnotification = None    # Reset notification counter.
+            
+            else:
+                self.newmail = False # If we have no mail OR had mail last time we checked,
+                                     # then there is no new mail.
+
             self.lastchecked = arrow.utcnow()
         return error
 
@@ -62,6 +84,7 @@ class Mailbox(object):
     def notify_owner(self, Owner):
         sendemail('OlinGotMail@gmail.com', self.owner.email, 'You\'ve Got Mail!', 'Go check your mailbox, ' + self.owner.name, \
              mail_auth['login'], mail_auth['password'])
+        self.lastnotification = arrow.utcnow()
 
 class Owner(object):
     def __init__(self, name, email):
@@ -87,12 +110,13 @@ def main():
             print 'Checking mailboxes...'
             for mb in Olin.mailboxes:   # Go to every mailbox,
                 error = mb.check()      # And check for mail
+                print mb.got_mail
                 if error:   # Fiddlesticks
                     print 'Error while checking ' + str(mb)
             print 'Done Checking.'
 
         # For every mailbox
-            #if it's got mail AND NOT within
+            #if it's got mail AND NOT within a few days since last notification
 
 
 
