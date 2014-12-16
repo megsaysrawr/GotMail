04be5172078929cfae3d7e073541b2acaa6480f4
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2014-12-15 22:11:08 -0500

    This works
    New template.

--- FlaskTest_1/FlaskTest_1/routes.py
+++ FlaskTest_1/FlaskTest_1/routes.py
@@ -7,25 +7,11 @@ from spark import sparkcheck
 @app.route('/')
 @app.route('/home')
 def home():
-    sparkserv = SparkCloud('accesstoken')  # Connect to Spark cloud
-    core = sparkserv.RE_core1
-    try:
-        sensor = core.analogRead('1')
-    except:     # Specifically, KeyError, I think.
-        # Catch errors, spark temporarily disconnected from internet, etc.
-        # print 'Connection error'
-        got_mail_ret = 2
-    else:
-        if sensor > 3300:   # Mail threshold
-            got_mail_ret = 1
-        else:
-            got_mail_ret = 0
-
-    responses = ['There is no mail.', 'You\'ve got mail!', 'There was a connectivity error. Please try again later.']
+    
     return render_template(
         'index.html',
         title = 'Home Page',
-        message = responses[got_mail_ret],
+        message = 'Message goes here!',
     )
 
 @app.route('/contact')
