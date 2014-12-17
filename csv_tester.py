032dfa2b35911e2469271a410189c6199c7dd96e
Anders Johnson <soccerajohnson@gmail.com>
2014-12-16 23:25:43 -0500

    Can now read and write to csv files

--- csv_tester.py
+++ csv_tester.py
@@ -1,9 +1,9 @@
 import csv
 
-with open('sample_csv.csv', 'wb') as csvfile:
+with open('sample_csv.csv', 'r+b') as csvfile:
 	filewrite = csv.writer(csvfile, delimiter=',', \
 							quotechar='|', quoting=csv.QUOTE_MINIMAL)
-	number = [1,2,3,4,5]
-	status = [True,False,True,True,False]
+	number = [1,2,3,4,5,6]
+	status = [True,False,True,True,False,True]
 	filewrite.writerow(number)
 	filewrite.writerow(status)
