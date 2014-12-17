3fb210775bc7a0a7115505680d44a646100e1b15
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2014-12-17 17:42:16 -0500

    Mailbox selection works!

--- FlaskTest_1/FlaskTest_1/forms.py
+++ FlaskTest_1/FlaskTest_1/forms.py
@@ -1,6 +1,12 @@
 from flask.ext.wtf import Form, html5
-from wtforms import StringField, BooleanField
+from wtforms import StringField, BooleanField, IntegerField
 from wtforms.validators import DataRequired, NumberRange
 
 class MailboxForm(Form):
-    num_box = html5.IntegerField('Mailbox Number', NumberRange(1, 10, 'Please enter your mailbox number (1-10)'))
+    num_box = IntegerField('Mailbox Number', [NumberRange(min=1, max=10, message='Please enter your mailbox number (1-10)')])
+    remember_me = BooleanField('remember_me', default=False)
+
+
+class LoginForm(Form):
+    openid = IntegerField('User Level', [NumberRange(min=0, max=10)])
+    remember_me = BooleanField('remember_me', default=False)
--- FlaskTest_1/FlaskTest_1/routes.py
+++ FlaskTest_1/FlaskTest_1/routes.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from flask import render_template, flash, redirect, request
 from app import app
-from forms import MailboxForm
+from forms import MailboxForm, LoginForm
+
 import requests as reqs
 import csv
 import arrow
@@ -9,16 +10,18 @@ import arrow
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
-@app.route('/olin/mb<mbnum>')
-@app.route('/olin/<mbnum>')
-def home(mbnum = 0):
-    form = MailboxForm
+@app.route('/olin/mb<mbnum>', methods=['GET', 'POST'])
+@app.route('/olin/<mbnum>', methods=['GET', 'POST'])
+def home(mbnum=0):
+    form = MailboxForm()
+    if form.validate_on_submit():
+        return redirect('/olin/'+str(form.num_box.data))
     def gotmail(mbnum):
         if mbnum == 0:
             return 'Please enter your mailbox url.'
         req = reqs.get('https://dl.dropboxusercontent.com/s/6lmwzacom7gdu0z/mailboxdata.csv')
         mbs = req.content.split()
-        got_mail = mbs[mbnum-1]
+        got_mail = mbs[mbnum - 1]
         responses = ["You've got mail!", "No mail today.", "This mailbox is not currently using Olin GotMail. Sign up!", "An error has occurred. Sorry!"]
         if got_mail == 'True':
             return responses[0]
@@ -29,46 +32,32 @@ def home(mbnum = 0):
         else:
             return responses[3]
     msg = gotmail(int(mbnum))
-    #timestp = arrow.utcnow().to('US/Eastern').format('h:mm:ssA on dddd, M/D/YYYY')
-    return render_template(
-        'index.html',
+    return render_template('index.html',
         timest = arrow.utcnow().replace(hours = -5).format('h:mm:ss A on dddd, M/D/YYYY'),
         title = 'Home Page',
         message = msg,
-    )
+        form = form)
 
 @app.route('/contact')
 def contact():
-    return render_template(
-        'contact.html',
+    return render_template('contact.html',
         title = 'Contact',
         year = datetime.now().year,
         applicationname = 'Check Mail',
-        message = 'Your contact page.'
-    )
+        message = 'Your contact page.')
 
 @app.route('/about')
 def about():
-    return render_template(
-        'about.html',
+    return render_template('about.html',
         title = 'About',
         year = datetime.now().year,
         applicationname = 'Check Mail',
-        message = 'Your application description page.'
-    )
+        message = 'Your application description page.')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/home')
-    return render_template(
-        'login.html', 
-         title='Sign In',
-         year = datetime.now().year,
-         applicationname = 'Check Mail',
-         message = 'Your application description page.',
-         form=form)
+    return render_template('login.html', 
+                           title='Sign In',
+                           form=form)
 
